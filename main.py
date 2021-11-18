3e70281166767e68d3e76130988f1e1c4d46823d
RougeVenne <feehumbrella@gmail.com>
2021-11-18 18:31:10 -0300

    nasceu

---
+++ main.py
@@ -0,0 +1,20 @@
+import discord, json
+
+with open("token.json") as tokenJson:
+    TOKEN = json.load(tokenJson)
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(TOKEN)
