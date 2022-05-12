2e4d0f773f7edbd16f9852fd8d0bd4e176fd4405
GleicianeSilva <gleicinhasilva@hotmail.com>
2022-05-12 16:57:47 -0300

    Visualizar calendario aulas no discord

--- discord_bot.py
+++ discord_bot.py
@@ -1,5 +1,6 @@
 import discord
 import json
+from calendario import Calendario 
 
 client = discord.Client()
 
@@ -12,9 +13,13 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-
-with open('token.json', 'r') as token_json:
+    classes = Calendario().main()
+    if message.content.startswith('$aula'):
+        await message.channel.send(f'Próxima aula: {classes[0]}\nVocê quer as próximas datas? Digite "$datas".')
+    if message.content.startswith('$datas'):
+        await message.channel.send(f'{classes[1]}\n{classes[2]}\n{classes[3]}\n')
+   
+   
+with open('discord_token.json', 'r') as token_json:
     TOKEN = json.load(token_json)
 client.run(TOKEN)
